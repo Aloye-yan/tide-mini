ccec09b8795b81edf2d59952b39eea06ab945f05
yan <17315533351@163.com>
2021-09-17 16:05:27 +0800

    change page

---
+++ pages/registration/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="registration">
+		<web-view
+			:src="`https://xzt.eastfair.com/regist/21/202110MusicEz/index.html#/home?ExhID=2714&InviteType=Platform&InviteCode=${inviteCode}&SpOpenID=${realUnionID}`">
+		</web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				inviteCode: "musiclab",
+				realUnionID: "",
+			}
+		},
+		onLoad() {
+
+		},
+		onShow() {
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.registration {}
+</style>
